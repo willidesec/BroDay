3bb30379036b5627b23378e01868736bb53b4cdb
William Désécot <william.desecot@hotmail.fr>
2020-11-23 12:32:20 +0100

    feat: create Xcode project

---
+++ BroDay/BroDay/BroDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BroDayApp.swift
+//  BroDay
+//
+//  Created by William DESECOT on 23/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct BroDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
